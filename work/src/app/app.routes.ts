409ffa8590d7e4f1ab6055e148986f0b0fbb45d3
DESKTOP-8JCV0GC\RAZAK <you@example.com>
2024-01-27 23:03:12 +0530

    new changes

--- work/src/app/app.routes.ts
+++ work/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '**', component: PagenotfoundComponent }
+    { path: 'dashboard', component: DashboardComponent },
+
+    { path: '**', component: PagenotfoundComponent }
 ];
